dee1e05eac84cd5fa88ce37240baa115854b05ac
lokerwr <boomwiser@gmail.com>
2026-08-29 08:51:09 +0100

    Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p>© {new Date().getFullYear()} Blue Wave. Worldwide shipping by air, land &amp; sea.</p>
+    </footer>
+  )
+}
